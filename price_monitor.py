3ec0517fd9655a16d4c6086fb341bb8d8d61ca3f
Strydex <143320768+strydex@users.noreply.github.com>
2024-06-17 02:37:27 +0300

    Heavy update of price_monitor.py
    Rewrite massive part of the code to comply TG limits and add randomness as well as setting time zones

--- price_monitor.py
+++ price_monitor.py
@@ -1,108 +1,62 @@
 import asyncio
 import config
 from bybit_client import BybitClient
-from telegram_bot import send_message, send_photo
+from telegram_bot import send_message
 import matplotlib.pyplot as plt
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.bot import DefaultBotProperties
 import time
 from datetime import datetime, timedelta
+import pytz
+import random
 
 TOKEN = config.TELEGRAM_BOT_TOKEN
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
 dp = Dispatcher(storage=MemoryStorage())
 
-# Словарь для отслеживания состояния пользователей, времени последней отправки сообщения и количества сообщений
-user_states = {}
-last_message_time = {}
-message_count = {}
-monitoring_task = None
-
 # Инициализация клиента Bybit
 client = BybitClient(config.BYBIT_API_KEY, config.BYBIT_API_SECRET)
 
-# Интервал между сообщениями в секундах (5 минут = 300 секунд)
-MESSAGE_INTERVAL = 2
+# Интервал между сообщениями в секундах (50 минут = 3000 секунд)
+MESSAGE_INTERVAL = 3000
 # Лимит сообщений в день
-DAILY_MESSAGE_LIMIT = 3
+DAILY_MESSAGE_LIMIT = 18
 
-async def start_monitoring():
-    global monitoring_task
-    if monitoring_task is None or monitoring_task.done():
-        monitoring_task = asyncio.create_task(monitor_market())
+# Устанавливаем временную зону для корректной проверки времени
+timezone = pytz.timezone('Europe/Moscow')
 
-async def stop_monitoring():
-    global monitoring_task
-    if monitoring_task and not monitoring_task.done():
-        monitoring_task.cancel()
-        try:
-            await monitoring_task
-        except asyncio.CancelledError:
-            pass
-
-@dp.message(Command("start"))
-async def start(message: types.Message):
-    user_id = message.from_user.id
-    user_states[user_id] = True  # Отмечаем, что бот активен для этого пользователя
-    message_count[user_id] = {'count': 0, 'reset_time': datetime.now() + timedelta(days=1), 'limit_reached': False}
-    await message.reply("Бот начал работу!")
-    await start_monitoring()
-
-@dp.message(Command("stop"))
-async def stop(message: types.Message):
-    user_id = message.from_user.id
-    user_states[user_id] = False  # Отмечаем, что бот неактивен для этого пользователя
-    await message.reply("Бот остановлен!")
-    await stop_monitoring()
-
-async def monitor_market():
+async def monitor_market(last_message_time, message_count, message_count_reset_time):
     while True:
-        current_time = time.time()
-        for user_id, is_active in user_states.items():
-            if is_active:
-                if user_id not in last_message_time or current_time - last_message_time[user_id] >= MESSAGE_INTERVAL:
-                    if message_count[user_id]['count'] < DAILY_MESSAGE_LIMIT:
-                        tickers = client.get_tickers()
-                        for ticker in tickers:
-                            symbol = ticker['symbol']
-                            price_change = client.get_price_change(symbol)
-                            if abs(price_change) > config.PRICE_CHANGE_THRESHOLD:
-                                message = f"🚨<b>Crypto Alert!</b>🚨\n\n<b>Symbol:</b> {symbol}\n<b>Price Change:</b> {price_change:.2f}%"
-                                await bot.send_message(user_id, message)
-                                last_message_time[user_id] = current_time
-                                message_count[user_id]['count'] += 1
-                                message_count[user_id]['limit_reached'] = False
-                                break  # Отправляем только одно сообщение за интервал времени
-                    else:
-                        reset_time = message_count[user_id]['reset_time']
-                        if datetime.now() >= reset_time:
-                            message_count[user_id] = {'count': 0, 'reset_time': datetime.now() + timedelta(days=1), 'limit_reached': False}
-                        elif not message_count[user_id]['limit_reached']:
-                            await bot.send_message(user_id, "Это последние новости на сегодня, в данный момент лимит получения новостей достигнут.")
-                            message_count[user_id]['limit_reached'] = True
-        await asyncio.sleep(config.CHECK_INTERVAL)
-
-@dp.message(F.text)
-async def handle_message(message: types.Message):
-    user_id = message.from_user.id
-    if user_states.get(user_id, False):
-        await message.reply("Бот активен и получил ваше сообщение!")
-    else:
-        await message.reply("Бот неактивен. Используйте /start для активации.")
-
-async def set_commands(bot: Bot):
-    commands = [
-        types.BotCommand(command="/start", description="Запустить бота"),
-        types.BotCommand(command="/stop", description="Остановить бота"),
-    ]
-    await bot.set_my_commands(commands)
+        current_time = datetime.now(timezone)
+        if current_time - last_message_time >= timedelta(seconds=MESSAGE_INTERVAL):
+            if message_count < DAILY_MESSAGE_LIMIT:
+                try:
+                    tickers = client.get_tickers()
+                    random_ticker = random.choice(tickers)
+                    symbol = random_ticker['symbol']
+                    price_change = client.get_price_change(symbol)
+                    if abs(price_change) > config.PRICE_CHANGE_THRESHOLD:
+                        message = f"🚨<b>Crypto Alert!</b>🚨\n\n<b>Тикер:</b> {symbol}\n<b>Изменение цены:</b> {price_change:.2f}%"
+                        await bot.send_message(chat_id=config.TELEGRAM_CHANNEL_ID, text=message, parse_mode='HTML')
+                        print(f"Sent message: {message}")  # Добавил эту Line для логов в консоли
+                        last_message_time = current_time
+                        message_count += 1
+                except Exception as e: # Обрабатываем исключения
+                    print(f"An error occurred: {e}")
+            else:
+                if current_time >= message_count_reset_time:
+                    message_count = 0
+                    message_count_reset_time = current_time + timedelta(days=1)
+        await asyncio.sleep(config.CHECK_INTERVAL) #Регулируем дилэй запросов
 
 async def main():
-    await set_commands(bot)
-    # Запускаем поллинг
-    await dp.start_polling(bot)
+    # Запускаем TG поллинг
+    polling_task = asyncio.create_task(dp.start_polling(bot))
+    # Запускаем мониторинг рынка
+    monitoring_task = asyncio.create_task(monitor_market(datetime.now(timezone), 0, datetime.now(timezone) + timedelta(days=1)))
+    # Ждем завершения обеих задач
+    await asyncio.gather(polling_task, monitoring_task)
 
 if __name__ == '__main__':
     asyncio.run(main())
