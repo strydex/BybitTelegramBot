6f48048fae290562ef12f76bef4d7350e291494d
Strydex <143320768+strydex@users.noreply.github.com>
2024-06-17 02:40:52 +0300

    Update config.py
    Removal of a useless variable

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 BYBIT_API_KEY = 'PUT YOUR API KEY HERE'
 BYBIT_API_SECRET = 'PUT YOUR API SECRET HERE'
 TELEGRAM_BOT_TOKEN = 'PUT YOUR BOT TOKEN HERE'
-TELEGRAM_CHAT_ID = 'PUT YOUR TELEGRAM CHAT ID HERE'
-TELEGRAM_CHANNEL_ID = 'PUT YOUR TELEGRAM CHAT ID HERE' # Используем эту переменную для отправки в канал вместо чата. 
-CHECK_INTERVAL = 5 # Интервал проверки в секундах
+TELEGRAM_CHANNEL_ID = 'PUT YOUR TELEGRAM CHANNEL ID HERE' # Используем ID канала из Веб-версии. 
+CHECK_INTERVAL = 300 # Интервал проверки в секундах
 PRICE_CHANGE_THRESHOLD = 10  # Порог изменения цены в процентах
