7f017f84a68729129d121c5dd22786c243295b46
Strydex <143320768+strydex@users.noreply.github.com>
2024-06-16 17:41:46 +0300

    Update config.py
    A slight update for posting to a Telegram Channel instead of chats

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ BYBIT_API_KEY = 'PUT YOUR API KEY HERE'
 BYBIT_API_SECRET = 'PUT YOUR API SECRET HERE'
 TELEGRAM_BOT_TOKEN = 'PUT YOUR BOT TOKEN HERE'
 TELEGRAM_CHAT_ID = 'PUT YOUR TELEGRAM CHAT ID HERE'
+TELEGRAM_CHANNEL_ID = 'PUT YOUR TELEGRAM CHAT ID HERE' # Используем эту переменную для отправки в канал вместо чата. 
 CHECK_INTERVAL = 5 # Интервал проверки в секундах
 PRICE_CHANGE_THRESHOLD = 10  # Порог изменения цены в процентах
