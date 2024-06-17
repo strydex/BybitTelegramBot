be85330b5deffbaa27e4cbdfd0e4c43d75469a2d
Strydex <143320768+strydex@users.noreply.github.com>
2024-06-17 05:06:15 +0300

    Update price_monitor.py
    Translation to Ru in notifications

--- price_monitor.py
+++ price_monitor.py
@@ -1,15 +1,15 @@
 import asyncio
 import config
 from bybit_client import BybitClient
-from telegram_bot import send_message
+from telegram_bot import send_message, send_photo
 import matplotlib.pyplot as plt
-from aiogram import Bot, Dispatcher, F, types
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.bot import DefaultBotProperties
 import time
 from datetime import datetime, timedelta
 import pytz
-import random
 
 TOKEN = config.TELEGRAM_BOT_TOKEN
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
@@ -18,12 +18,12 @@ dp = Dispatcher(storage=MemoryStorage())
 # Инициализация клиента Bybit
 client = BybitClient(config.BYBIT_API_KEY, config.BYBIT_API_SECRET)
 
-# Интервал между сообщениями в секундах (50 минут = 3000 секунд)
-MESSAGE_INTERVAL = 3000
+# Интервал между сообщениями в секундах (5 минут = 300 секунд)
+MESSAGE_INTERVAL = 300
 # Лимит сообщений в день
-DAILY_MESSAGE_LIMIT = 18
+DAILY_MESSAGE_LIMIT = 30
 
-# Устанавливаем временную зону для корректной проверки времени
+# Устанавливаем временную зону
 timezone = pytz.timezone('Europe/Moscow')
 
 async def monitor_market(last_message_time, message_count, message_count_reset_time):
@@ -33,25 +33,25 @@ async def monitor_market(last_message_time, message_count, message_count_reset_t
             if message_count < DAILY_MESSAGE_LIMIT:
                 try:
                     tickers = client.get_tickers()
-                    random_ticker = random.choice(tickers)
-                    symbol = random_ticker['symbol']
-                    price_change = client.get_price_change(symbol)
-                    if abs(price_change) > config.PRICE_CHANGE_THRESHOLD:
-                        message = f"🚨<b>Crypto Alert!</b>🚨\n\n<b>Тикер:</b> {symbol}\n<b>Изменение цены:</b> {price_change:.2f}%"
-                        await bot.send_message(chat_id=config.TELEGRAM_CHANNEL_ID, text=message, parse_mode='HTML')
-                        print(f"Sent message: {message}")  # Добавил эту Line для логов в консоли
-                        last_message_time = current_time
-                        message_count += 1
-                except Exception as e: # Обрабатываем исключения
+                    for ticker in tickers:
+                        symbol = ticker['symbol']
+                        price_change = client.get_price_change(symbol)
+                        if abs(price_change) > config.PRICE_CHANGE_THRESHOLD:
+                            message = f"🚨<b>Crypto Alert!</b>🚨\n\n<b>Тикер:</b> {symbol}\n<b>Изменение цены:</b> {price_change:.2f}%"
+                            await bot.send_message(chat_id=config.TELEGRAM_CHANNEL_ID, text=message, parse_mode='HTML')
+                            last_message_time = current_time
+                            message_count += 1
+                            break  # Send only one message per interval
+                except Exception as e:
                     print(f"An error occurred: {e}")
             else:
                 if current_time >= message_count_reset_time:
                     message_count = 0
                     message_count_reset_time = current_time + timedelta(days=1)
-        await asyncio.sleep(config.CHECK_INTERVAL) #Регулируем дилэй запросов
+        await asyncio.sleep(config.CHECK_INTERVAL)
 
 async def main():
-    # Запускаем TG поллинг
+    # Запускаем поллинг
     polling_task = asyncio.create_task(dp.start_polling(bot))
     # Запускаем мониторинг рынка
     monitoring_task = asyncio.create_task(monitor_market(datetime.now(timezone), 0, datetime.now(timezone) + timedelta(days=1)))
